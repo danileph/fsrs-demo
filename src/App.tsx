fccaffec17f57e49e318b97d3814a32b3362aa06
Danil Efremov <danil_efremov1@rntgroup.com>
2024-12-18 20:20:13 +0300

    fix: fix eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from "react-router-dom"
-import { Layout } from "./components/layout"
 import { router } from "./lib/consts/router"
 
 function App() {
--- src/pages/statistics.tsx
+++ src/pages/statistics.tsx
@@ -1,5 +1,5 @@
-import { FC, useEffect, useState } from "react"
-import { CreateKnowledgeCard, KnowledgeCard, KnowledgeLineChart, MutateKnowledgeWindow, useGetKnowledges } from "../components/knowledge";
+import { FC } from "react"
+import { KnowledgeLineChart, useGetKnowledges } from "../components/knowledge";
 import { Layout } from "../components/layout/ui";
 
 export interface IStatisticsPageProps extends React.HTMLAttributes<HTMLDivElement> {}
